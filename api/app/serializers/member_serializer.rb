93af10ba56ac50b089ea5aa9ca92ee3fe26187a2
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-01-12 17:10:30 +0000

    API changes to allow CORS

--- api/app/serializers/member_serializer.rb
+++ api/app/serializers/member_serializer.rb
@@ -1,7 +1,3 @@
 class MemberSerializer < ActiveModel::Serializer
-  attributes :full_name, :dob, :gender
-
-  def full_name
-    "#{object.first_name} #{object.last_name}"
-  end
+  attributes :id, :first_name, :last_name, :dob, :gender, :created_at, :updated_at
 end
