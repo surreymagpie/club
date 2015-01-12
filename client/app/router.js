597733ffeeb49bab3c6e9cd28a48f12f4482849f
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-01-12 17:12:49 +0000

    Client-side member resource

--- client/app/router.js
+++ client/app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('members', function() {
+    this.resource('member', { path: '/:member_id' });
+  });
 });
 
 export default Router;
